989614e40ef0618a1dc53194d6ddf1f1192fc3d0
Fleford Redoloza <fleford@gmail.com>
2019-03-24 15:46:58 -0600

    added the shift_matrix function

--- LaplaceSmoother.py
+++ LaplaceSmoother.py
@@ -1,6 +1,24 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-h_field = np.loadtxt("InputFolder/initial_heads.txt")
+# h_field = np.loadtxt("InputFolder/initial_heads.txt")
 k_field = np.loadtxt("InputFolder/k_field.txt")
+h_field = np.ones((10, 20))
+
+
+def shift_matrix(matrix, direction):
+    # Shifts a 2d matrix in a direction and pads with zeros
+    if direction == "up":
+        return np.delete(np.pad(matrix, ((0, 1), (0, 0)), 'constant'), 0, 0)
+    elif direction == "down":
+        return np.delete(np.pad(matrix, ((1, 0), (0, 0)), 'constant'), -1, 0)
+    elif direction == "left":
+        return np.delete(np.pad(matrix, ((0, 0), (0, 1)), 'constant'), 0, 1)
+    elif direction == "right":
+        return np.delete(np.pad(matrix, ((0, 0), (1, 0)), 'constant'), -1, 1)
+    else:
+        return "NO DIRECTION SPECIFIED: up, down, left, right"
+
 
 print(h_field)
+print(shift_matrix(h_field, "right"))
