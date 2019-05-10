8f7340902f13d894328283d0ff351ada3d1a5f39
Fleford Redoloza <fleford@gmail.com>
2019-05-09 18:07:08 -0700

    workin on func to convert input matrix to multiple matrices

--- LaplaceSmoother.py
+++ LaplaceSmoother.py
@@ -211,14 +211,34 @@ def input_matrix_to_parameter_matrices(input_matrix):
     # (x = 1) = active cells
     # (x > 1) = observed head (assumed an active cell)
 
+    # k_field_cnst_bnd
+    k_field_cnst_bnd = input_matrix.copy()
+    k_field_cnst_bnd = np.clip(k_field_cnst_bnd, -1, 1)
+    # print(k_field_cnst_bnd)
+
+    # k_field_cnst_obs
+    k_field_cnst_obs = input_matrix.copy()
+    k_field_cnst_obs = np.absolute(k_field_cnst_obs)
+    k_field_cnst_obs[k_field_cnst_obs > 1] = -1
+    # print(k_field_cnst_obs)
+
+    # obs_field
+    obs_field = input_matrix.copy()
+    obs_field[obs_field <= 1] = 0
+    print(obs_field)
+
+
 
     return None
 
 
+# Load in initial input file
+initial_input = np.loadtxt("InputFolder/initial_input.txt")
+input_matrix_to_parameter_matrices(initial_input)
+
 # Load in observation values
 obs_field = np.loadtxt("InputFolder/initial_heads.txt")
-
-h_field = obs_field
+h_field = obs_field.copy()
 # h_field = np.ones((10, 20))*10
 # Load k_field with parameters
 k_field_const_obs = np.loadtxt("InputFolder/k_field.txt")
@@ -244,7 +264,7 @@ k_field2_new = calculate_new_k_field(initial_h_field, k_field2, obs_field)
 # run gw_model with old and new k_field
 h_with_old_k = laplace_smooth_iter(initial_h_field, k_field2)
 h_with_new_k = laplace_smooth_iter(initial_h_field, k_field2_new)
-print(h_with_old_k)
-print(h_with_new_k)
-plt.matshow(h_with_new_k - h_with_old_k)
-plt.show()
+# print(h_with_old_k)
+# print(h_with_new_k)
+# plt.matshow(h_with_new_k - h_with_old_k)
+# plt.show()
