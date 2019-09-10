e798ad14df540c91fefe706b033240cb005004ff
Fleford <fleford@gmail.com>
2019-09-10 16:40:39 -0600

    fixed bug with zero columns and rows

--- LinAlg_Laplacian_v2.py
+++ LinAlg_Laplacian_v2.py
@@ -3,6 +3,7 @@ from scipy.sparse.linalg import spsolve
 from scipy.sparse import csc_matrix
 import matplotlib.pyplot as plt
 import time
+from numba import njit, prange
 np.set_printoptions(linewidth=300)
 
 
@@ -20,11 +21,53 @@ def shift_matrix(matrix, direction):
         return "NO DIRECTION SPECIFIED: up, down, left, right"
 
 
+# @njit()
+def generate_laplace_operator_matrix(in_k_vector, in_k_vector_sum, in_k_field_height, in_k_field_width):
+    # Prep laplace_operator_matrix
+    laplace_op_matrix = np.zeros_like(in_k_vector)  # Seed matrix with empty array
+    for row in range(in_k_field_height):
+        for col in range(in_k_field_width):
+            # Prep a new row
+            laplace_operator_row = np.zeros_like(in_k_vector)
+
+            # Prep index (Zero out row if k is zero)
+            index = row * in_k_field_width + col
+
+            if in_k_vector[0, index] == 0:
+                laplace_op_matrix = np.concatenate((laplace_op_matrix, laplace_operator_row))
+                continue
+
+            # Mark center cell
+            laplace_operator_row[0, index] = in_k_vector_sum[0, index]
+
+            # Mark side cells
+            if 0 <= (row + 1) <= (in_k_field_height - 1):
+                index_up = (row + 1) * in_k_field_width + col
+                laplace_operator_row[0, index_up] = - in_k_vector[0, index_up]
+            if 0 <= (row - 1) <= (in_k_field_height - 1):
+                index_down = (row - 1) * in_k_field_width + col
+                laplace_operator_row[0, index_down] = - in_k_vector[0, index_down]
+            if 0 <= (col - 1) <= (in_k_field_width - 1):
+                index_right = row * in_k_field_width + (col - 1)
+                laplace_operator_row[0, index_right] = - in_k_vector[0, index_right]
+            if 0 <= (col + 1) <= (in_k_field_width - 1):
+                index_left = row * in_k_field_width + (col + 1)
+                laplace_operator_row[0, index_left] = - in_k_vector[0, index_left]
+
+            # Append row to matrix
+            laplace_op_matrix = np.concatenate((laplace_op_matrix, laplace_operator_row))
+
+    # Cut off empty top row
+    laplace_op_matrix = np.delete(laplace_op_matrix, np.arange(k_field_cnt)).reshape(-1, k_field_cnt)
+
+    return laplace_op_matrix
+
+print("hup")
 # k_field = np.loadtxt("InputFolder/k_field_tiny.txt")
-k_field = np.loadtxt("InputFolder/k_field.txt")
+k_field = np.loadtxt("InputFolder/k_field_sv_230.txt")
 k_field_abs = np.abs(k_field)
 # h_field_initial = np.loadtxt("InputFolder/h_field_tiny.txt")
-h_field_initial = np.loadtxt("InputFolder/initial_heads.txt")
+h_field_initial = np.loadtxt("InputFolder/h_field_sv_230.txt")
 
 start_time = time.time()
 
@@ -37,7 +80,7 @@ k_field_cnt = k_field_width * k_field_height
 # print(h_field_initial)
 # print()
 
-
+print("hup")
 # Prepare k_vectors and h_vector
 k_vector_signed = k_field.reshape(1, -1)
 k_vector = k_field_abs.reshape(1, -1)
@@ -47,7 +90,7 @@ k_vector_left = shift_matrix(k_field_abs, "left").reshape(1, -1)
 k_vector_right = shift_matrix(k_field_abs, "right").reshape(1, -1)
 k_vector_sum = k_vector_up + k_vector_down + k_vector_left + k_vector_right
 h_vector = h_field_initial.reshape(1, -1)
-
+print("hup")
 # print(k_vector_signed)
 # print(k_vector)
 # print(k_vector_up)
@@ -59,42 +102,9 @@ h_vector = h_field_initial.reshape(1, -1)
 # print()
 
 # Prep laplace_operator_matrix
-laplace_operator_matrix = np.zeros_like(k_vector)  # Seed matrix with empty array
-for row in range(k_field_height):
-    for col in range(k_field_width):
-        # Prep a new row
-        laplace_operator_row = np.zeros_like(k_vector)
+laplace_operator_matrix = generate_laplace_operator_matrix(k_vector, k_vector_sum, k_field_height, k_field_width)
 
-        # Prep index (Zero out row if k is zero)
-        index = row * k_field_width + col
-
-        if k_vector[0, index] == 0:
-            laplace_operator_matrix = np.concatenate((laplace_operator_matrix, laplace_operator_row))
-            continue
-
-        # Mark center cell
-        laplace_operator_row[0, index] = k_vector_sum[0, index]
-
-        # Mark side cells
-        if 0 <= (row + 1) <= (k_field_height - 1):
-            index_up = (row + 1) * k_field_width + col
-            laplace_operator_row[0, index_up] = - k_vector[0, index_up]
-        if 0 <= (row - 1) <= (k_field_height - 1):
-            index_down = (row - 1) * k_field_width + col
-            laplace_operator_row[0, index_down] = - k_vector[0, index_down]
-        if 0 <= (col - 1) <= (k_field_width - 1):
-            index_right = row * k_field_width + (col - 1)
-            laplace_operator_row[0, index_right] = - k_vector[0, index_right]
-        if 0 <= (col + 1) <= (k_field_width - 1):
-            index_left = row * k_field_width + (col + 1)
-            laplace_operator_row[0, index_left] = - k_vector[0, index_left]
-
-        # Append row to matrix
-        laplace_operator_matrix = np.concatenate((laplace_operator_matrix, laplace_operator_row))
-
-# Cut off empty top row
-laplace_operator_matrix = np.delete(laplace_operator_matrix, np.arange(k_field_cnt)).reshape(-1, k_field_cnt)
-
+print("hup")
 # print(laplace_operator_matrix)
 # print()
 
@@ -108,7 +118,7 @@ for cell in range(k_field_cnt):
         constant_head_A_row[0, cell] = 1
         laplace_operator_matrix[cell, :] = constant_head_A_row
         constant_head_vector[cell, 0] = h_vector[0, cell]
-
+print("hup")
 # Clear out zero rows and columns
 # print(laplace_operator_matrix)
 # print(laplace_operator_matrix.shape)
